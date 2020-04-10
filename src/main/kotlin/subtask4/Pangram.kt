6723a5cd2e1ff2399adcb4d98521fc7c2f5de26c
Tibeikin Oleg <tibidcp@gmail.com>
2020-04-11 01:04:45 +0300

    add Pangram

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,37 @@
 package subtask4
 
 class Pangram {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        //pangram
+        val vowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
+        val consonants = listOf(
+            'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q',
+            'r', 's', 't', 'v', 'x', 'z', 'w'
+        )
+        return when {
+            //pangram
+            inputString.toLowerCase().toSet().count { it in 'a'..'z' } == 26 -> {
+                convert(inputString, vowels)
+            }
+            //empty string
+            inputString.isBlank() -> {
+                ""
+            }
+            //not pangram
+            else -> {
+                convert(inputString, consonants)
+            }
+        }
     }
+
+    private fun convert(inputString: String, charList: List<Char>) =
+        inputString
+            .trim()
+            .split("\\s+".toRegex())
+            .sortedBy { word -> word.count { char -> char.toLowerCase() in charList } }
+            .joinToString(" ") { word ->
+                word.count { char -> char.toLowerCase() in charList }.toString() +
+                        word.map { char -> if (char.toLowerCase() in charList)
+                            char.toUpperCase() else char }.joinToString("")
+            }
 }
