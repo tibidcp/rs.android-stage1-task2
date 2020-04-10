4dbc580e3541c0253736a5bce913d9452818e479
Tibeikin Oleg <tibidcp@gmail.com>
2020-04-10 18:12:36 +0300

    add DateFormatter and TimePicker

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,23 @@
 package subtask1
 
-class DateFormatter {
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.*
 
-    // TODO: Complete the following function
+class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val yearInt = year.toInt()
+        val dayInt = day.toInt()
+        val monthInt = month.toInt()
+        if (monthInt > 12 || dayInt > YearMonth.of(yearInt, monthInt).lengthOfMonth()) {
+            return "Такого дня не существует"
+        }
+        val date = LocalDate.of(yearInt, monthInt, dayInt)
+        val dayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale("ru"))
+        val monthValue = date.month.getDisplayName(TextStyle.FULL, Locale("ru"))
+        return "$day $monthValue, $dayOfWeek"
     }
 }
--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,53 @@
 package subtask2
 
 class TimeConverter {
-
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val hourInt = hour.toInt()
+        val minuteInt = minute.toInt()
+
+        val numberToWord = mapOf(
+            1 to "one",
+            2 to "two",
+            3 to "three",
+            4 to "four",
+            5 to "five",
+            6 to "six",
+            7 to "seven",
+            8 to "eight",
+            9 to "nine",
+            10 to "ten",
+            11 to "eleven",
+            12 to "twelve",
+            13 to "thirteen",
+            14 to "fourteen",
+            15 to "fifteen",
+            16 to "sixteen",
+            17 to "seventeen",
+            18 to "eighteen",
+            19 to "nineteen",
+            20 to "twenty",
+            21 to "twenty one",
+            22 to "twenty two",
+            23 to "twenty three",
+            24 to "twenty four",
+            25 to "twenty five",
+            26 to "twenty six",
+            27 to "twenty seven",
+            28 to "twenty eight",
+            29 to "twenty nine"
+        )
+
+        return when (minuteInt) {
+            0 -> "${numberToWord[hourInt]} o' clock"
+            1 -> "one minute past ${numberToWord[hourInt]}"
+            in 2..14 -> "${numberToWord[minuteInt]} minutes past ${numberToWord[hourInt]}"
+            15 -> "quarter past ${numberToWord[hourInt]}"
+            in 16..29 -> "${numberToWord[minuteInt]} minutes past ${numberToWord[hourInt]}"
+            30 -> "half past ${numberToWord[hourInt]}"
+            in 31..44 -> "${numberToWord[60 - minuteInt]} minutes to ${numberToWord[hourInt + 1]}"
+            45 -> "quarter to ${numberToWord[hourInt + 1]}"
+            in 46..59 -> "${numberToWord[60 - minuteInt]} minutes to ${numberToWord[hourInt + 1]}"
+            else -> ""
+        }
     }
 }
