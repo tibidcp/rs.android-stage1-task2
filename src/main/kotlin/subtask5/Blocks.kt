d8b87677c5439f1b7484deb7a67c71127155623f
Tibeikin Oleg <tibidcp@gmail.com>
2020-04-11 14:45:53 +0300

    add Blocks and Fibonacci

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,24 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+        return when (blockB) {
+            String::class -> blockA
+                .filterIsInstance<String>()
+                .joinToString("")
+            Int::class -> blockA
+                .filterIsInstance<Int>()
+                .sum()
+            LocalDate::class -> {
+                blockA
+                    .filterIsInstance<LocalDate>()
+                    .max()?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")).toString()
+            }
+            else -> throw IllegalArgumentException("Wrong class")
+        }
     }
 }
--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -1,9 +1,18 @@
 package subtask6
 
 class Fibonacci {
-
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var n1 = 0
+        var n2 = 1
+        while (n > n1 * n2) {
+            val n3 = n1 + n2
+            n1 = n2
+            n2 = n3
+        }
+        return if (n == n1 * n2) {
+            intArrayOf(n1, n2, 1)
+        } else {
+            intArrayOf(n1, n2, 0)
+        }
     }
 }
